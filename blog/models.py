e5350d26b485004be5ca1375220d8df9c5102fd1
ksg-dev <ksg.dev.data@gmail.com>
2025-05-13 09:54:49 -0500

    feat: implement blog data models Create Post, Category, Tag models Add fields and relationships Define Meta options and string representations Add URL methods Refs: #12

--- blog/models.py
+++ blog/models.py
@@ -1,37 +1,88 @@
 from django.db import models
-
-# Create your models here.
+from django.urls import reverse
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=20)
-    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, max_length=100)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+        ordering = ['name']
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('blog:category', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=20)
     slug = models.SlugField(unique=True)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('blog:tag', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Tag, self).save(*args, **kwargs)
+
 
 class Post(models.Model):
-    title = models.CharField(max_length=75)
-    excerpt = models.CharField(max_length=150)
-    image = models.ImageField(upload_to="posts", null=True)
-    date = models.DateField(auto_now=True)
-    slug = models.SlugField(unique=True)
-    readtime = models.IntegerField()
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, max_length=200)
+    excerpt = models.TextField(blank=True)
+    featured_image = models.ImageField(
+        upload_to="blog/images/%Y/%m/%d/", null=True, blank=True)
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='draft')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    published = models.DateTimeField(blank=True, null=True)
+    reading_time = models.IntegerField(default=5)
     content = models.TextField()
     # Relationship Fields
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="posts"
+        )
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, related_name="posts"
-    )
-    tags = models.ManyToManyField(Tag)
+        Category, on_delete=models.CASCADE, null=True, related_name="posts"
+        )
+    tags = models.ManyToManyField(Tag, blank=True, related_name="posts")
+
+    class Meta:
+        ordering = ['-published']
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        if not self.excerpt and self.content:
+            self.excerpt = self.content[:200] + '...'
+        super(Post, self).save(*args, **kwargs)
