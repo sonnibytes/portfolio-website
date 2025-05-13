b1949026e08fc52850ddb0ff57cf685581ced68b
ksg-dev <ksg.dev.data@gmail.com>
2025-05-13 18:08:31 -0500

    feat: startapp for core and projects Refs: #13 #5

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,6 +37,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "markdownx",
     "blog.apps.BlogConfig",
+    "core.apps.CoreConfig",
+    "projects.apps.ProjectsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
