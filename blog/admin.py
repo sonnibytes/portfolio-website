5fb9e1781238590f599c71a709b173ee6afea957
ksg-dev <ksg.dev.data@gmail.com>
2025-05-13 10:19:24 -0500

    feat: configure blog admin interface Register models w admin site Customize list displays and filters Add search fields and date hierarchy Refs: #12

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Post, Category, Tag
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'created', 'published')
+    list_filter = ('status', 'created', 'published', 'author')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author')
+    date_hierarchy = 'published'
+    ordering = ('status', '-published')
