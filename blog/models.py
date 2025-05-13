8c54d73ea62dcd806b3488f449858224bbd2850f
ksg-dev <ksg.dev.data@gmail.com>
2025-05-13 09:32:28 -0500

    feat: build intitial blog model structure Refs: #12

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    excerpt = models.CharField(max_length=150)
+    image = models.ImageField(upload_to="posts", null=True)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    readtime = models.IntegerField()
+    content = models.TextField()
+    # Relationship Fields
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, related_name="posts"
+    )
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--fyi-1-&lmbe@eodje_&wb(ew_!)(47btf0!#d5_tidcd!-mgr"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
