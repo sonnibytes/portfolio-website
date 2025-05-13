9ccca6d523d9fc0030cfd356918b29fb0c90d903
ksg-dev <ksg.dev.data@gmail.com>
2025-05-13 10:13:12 -0500

    feat: add blog views Implement class-based views Set up context data Refs: #12

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,75 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, TemplateView
+from django.shortcuts import get_object_or_404
+from .models import Post, Category, Tag
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Post.objects.filter(status="published")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['tags'] = Tag.objects.all()
+        return context
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+    def get_queryset(self):
+        return Post.objects.filter(status="published")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['tags'] = Tag.objects.all()
+        return context
+
+
+class CategoryView(ListView):
+    model = Post
+    template_name = "blog/category.html"
+    context_object_name = "posts"
+    paginate_by = 10
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return Post.objects.filter(category=self.category, status="published")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.category
+        return context
+
+
+class TagView(ListView):
+    model = Post
+    template_name = "blog/tag.html"
+    context_object_name = "posts"
+    paginate_by = 10
+
+    def get_queryset(self):
+        self.tag = get_object_or_404(Tag, slug=self.kwargs['slug'])
+        return Post.objects.filter(tags=self.tag, status="published")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.tag
+        return context
+
+
+class ArchiveView(TemplateView):
+    template_name = "blog/archive.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(status="published")
+        return context
