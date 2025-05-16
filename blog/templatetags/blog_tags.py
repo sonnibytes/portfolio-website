dfc52e59a5a3bb5236bb336e731cf6a52432a3a8
ksg-dev <ksg.dev.data@gmail.com>
2025-05-16 17:48:56 -0500

    feat: add post form html Refs: #14

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -7,3 +7,15 @@ register = template.Library()
 def get_item(dictionary, key):
     """Get an item from a dictionary using the key."""
     return dictionary.get(key)
+
+
+@register.filter
+def get_field(form, field_name):
+    """Get a form field by name."""
+    return form[field_name]
+
+
+@register.filter
+def add(value, arg):
+    """Concatenate strings."""
+    return value + arg
