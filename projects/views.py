abfd8c9adbb5f9d641e6bab95e12d4c81d7fbe15
ksg-dev <ksg.dev.data@gmail.com>
2025-05-22 12:09:45 -0500

    feat: add system admin CRUD views Refs: #32

--- projects/views.py
+++ projects/views.py
@@ -14,7 +14,7 @@ from django.http import JsonResponse
 from django.utils import timezone
 
 from .models import SystemModule, SystemType, Technology, SystemFeature, SystemMetric
-from blog.models import Post
+from blog.models import Post, SystemLogEntry
 
 
 class SystemModuleListView(ListView):
@@ -166,3 +166,231 @@ class SystemModuleDetailView(DetailView):
             context['next_system'] = None
 
         return context
+
+
+class SystemTypeDetailView(DetailView):
+    """View for displaying all systems of a specific type."""
+
+    model = SystemType
+    template_name = "projects/system_type.html"
+    context_object_name = "system_type"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        system_type = self.object 
+
+        # Systems in this type
+        context['systems'] = SystemModule.objects.filter(
+            system_type=system_type,
+            status__in=['deployed', 'published']
+        ).order_by('-created')
+
+        # Stats for this system type
+        context['total_systems'] = context['systems'].count()
+        context['avg_completion'] = context['systems'].aggregate(
+            avg_completion=Avg('completion_percent')
+        )['avg_completion'] or 0
+
+        # Technologies used in this system type
+        context['common_technologies'] = Technology.objects.filter(
+            systems__system_type=system_type,
+            systems__status__in=['deployed', 'published']
+        ).annotate(
+            usage_count=Count('systems')
+        ).order_by('-usage_count')[:10]
+
+        return context
+
+
+class TechnologyDetailView(DetailView):
+    """View for displaying all systems using a specific technology."""
+
+    model = Technology
+    template_name = "projects/technology_detail.html"
+    context_object_name = "technology"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        technology = self.object 
+
+        # Systems using this tech
+        context['systems'] = SystemModule.objects.filter(
+            technologies=technology,
+            status__in=['deployed', 'published']
+        ).order_by('-created')
+
+        # Stats
+        context['total_systems'] = context['systems'].count()
+
+        # System types that use this tech
+        context['system_types'] = SystemType.objects.filter(
+            systems__technologies=technology,
+            systems__status__in=['deployed', 'published']
+        ).annotate(
+            systems_count=Count('systems')
+        ).distinct().order_by('-systems_count')
+
+        return context
+
+
+class SystemDashboardView(ListView):
+    """HUD-style dashboard overview of all systems."""
+
+    model = SystemModule
+    template_name = "projects/systems_dashboard.html"
+    context_object_name = "recent_systems"
+
+    def get_queryset(self):
+        return SystemModule.objects.filter(
+            status__in=['deployed', 'published']
+        ).order_by('-created')[:6]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Dashboard Stats
+        context['dashboard_stats'] = {
+            'total_systems': SystemModule.objects.filter(
+                status__in=['deployed', 'published']
+            ).count(),
+            'systems_in_development': SystemModule.objects.filter(
+                status='in_development'
+            ).count(),
+            'systems_testing': SystemModule.objects.filter(
+                status='testing'
+            ).count(),
+            'total_technologies': Technology.objects.count(),
+            'avg_completion': SystemModule.objects.aggregate(
+                avg_completion=Avg('completion_percent')
+            )['avg_completion'] or 0,
+        }
+
+        # System type distribution
+        context['system_type_stats'] = SystemType.objects.annotate(
+            systems_count=Count('systems', filter=Q(systems__status__in=['deployed', 'published']))
+        ).filter(systems_count__gt=0).order_by('-systems_count')
+
+        # Tech usuage stats
+        context['tech_usuage_stats'] = Technology.objects.annotate(
+            usage_count=Count('systems', filter=Q(systems__status__in=['deployed', 'published']))
+        ).filter(usage_count__gt=0).order_by('-usage_count')[:10]
+
+        # Recent system logs
+        context['recent_logs'] = SystemLogEntry.objects.select_related(
+            'post', 'systems'
+        ).order_by('-logged_at')[:5]
+
+        # Systems by status for HUD-display
+        context['systems_by_status'] = {}
+        for status_choice in SystemModule.STATUS_CHOICES:
+            status_key = status_choice[0]
+            context['systems_by_status'][status_key] = SystemModule.objects.filter(
+                status=status_key
+            ).count()
+
+        return context
+
+# ===================== ADMIN/MANAGEMENT VIEWS =====================
+
+class SystemModuleCreateView(LoginRequiredMixin, CreateView):
+    """Create a new system module."""
+
+    model = SystemModule
+    template_name = "projects/admin/system_form.html"
+    fields = [
+        'title', 'subtitle', 'system_type', 'description', 'features_overview',
+        'technical_details', 'complexity', 'priority', 'status', 'featured',
+        'technologies', 'github_url', 'live_url', 'demo_url',
+        'thumbnail', 'banner_image', 'start_date', 'end_date'
+    ]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, f"System '{form.instance.title}' created successfully!")
+        return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Create New System"
+        context['submit_text'] = "Create System"
+        return context
+
+
+class SystemModuleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """Update an existing system module."""
+
+    model = SystemModule
+    template_name = "projects/admin/system_form.html"
+    fields = [
+        "title",
+        "subtitle",
+        "system_type",
+        "description",
+        "features_overview",
+        "technical_details",
+        "challenges",
+        "future_enhancements",
+        "complexity",
+        "priority",
+        "status",
+        "featured",
+        "technologies",
+        "github_url",
+        "live_url",
+        "demo_url",
+        "documentation_url",
+        "completion_percent",
+        "performance_score",
+        "uptime_percentage",
+        "thumbnail",
+        "banner_image",
+        "featured_image",
+        "architecture_diagram",
+        "start_date",
+        "end_date",
+        "deployment_date",
+    ]
+
+    def test_func(self):
+        system = self.get_object()
+        return self.request.user == system.author or self.request.user.is_staff
+
+    def form_valid(self, form):
+        messages.success(self.request, f"System '{form.instance.title}' updated successfully!")
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f"Edit System: {self.object.title}"
+        context['submit_text'] = "Update System"
+        return context
+
+
+class SystemModuleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """Delete a system module."""
+
+    model = SystemModule
+    template_name = "projects/admin/system_confirm_delete.html"
+    success_url = reverse_lazy('projects:system_list')
+
+    def test_func(self):
+        system = self.get_object()
+        return self.request.user == system.author or self.request.user.is_staff
+
+    def delete(self, request, *args, **kwargs):
+        system = self.get_object()
+        messages.success(request, f"System '{system.title}' deleted successfully!")
+        return super().delete(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f"Delete System: {self.object.title}"
+
+        # Get related data that will be affected
+        context['related_logs'] = self.object.get_related_logs()
+        context['related_features'] = self.object.features.count()
+        context['related_images'] = self.object.images.count()
+
+        return context
+
+
