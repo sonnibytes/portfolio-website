6fa3d5627e8f4677af684ea19c322db7aa9c0274
ksg-dev <ksg.dev.data@gmail.com>
2025-05-23 23:37:41 -0500

    feat: blog detail, archive temps and style Refs: #23

--- blog/templatetags/nav_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.inclusion_tag("components/sub-nav.html", takes_context=True)
-def render_subnav(context, links):
-    return {"links": links}
--- templatetags/nav_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.inclusion_tag("templates/components/sub-nav.html", takes_context=True)
-def render_subnav(context, links):
-    return {
-        "links": links
-    }
