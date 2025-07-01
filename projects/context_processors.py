debb8b9cd02ab3a29b3f82415e1bc489066f6643
ksg-dev <ksg.dev.data@gmail.com>
2025-07-01 13:38:49 -0500

    clean up systems subnav

--- projects/context_processors.py
+++ projects/context_processors.py
@@ -1,4 +1,4 @@
-from projects.models import SystemModule, Technology
+from projects.models import SystemModule, Technology, SystemType
 from django.db.models import Count
 from django.urls import reverse
 from django.utils import timezone
@@ -11,9 +11,11 @@ def systems_context(request):
     all_systems = SystemModule.objects.all()
     active_systems = SystemModule.objects.filter(status__in=['deployed', 'published'])
     total_tech = Technology.objects.count()
+    system_types = SystemType.objects.all()
 
     return {
         'total_systems': all_systems.count(),
         'active_systems': active_systems.count(),
         'total_technologies': total_tech,
+        'total_system_types': system_types.count(),
     }
