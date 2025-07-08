1ab171e46cec9d956cb4f0b9c2797025711ea255
ksg-dev <ksg.dev.data@gmail.com>
2025-07-08 08:55:04 -0500

    testing sys control interface refs: #73

--- projects/urls.py
+++ projects/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
 
   
     path("systems/", views.EnhancedLearningSystemListView.as_view(), name="system_list"),
-    path("systems/<slug:slug>/", views.LearningSystemControlInterfaceView.as_view(), name="system_detail"),
+    # path("systems/<slug:slug>/", views.LearningSystemControlInterfaceView.as_view(), name="system_detail"),
+    # Testing new interface w GitHub Data
+    path("systems/<slug:slug>/", views.SystemControlInterfaceView.as_view(), name="system_detail"),
 
     # path("systems/dashboard/", views.SystemsDashboardView.as_view(), name="systems_dashboard"),
 
