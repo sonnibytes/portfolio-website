43dbe78c6701107f21c8e4084f24a56665b0ab11
ksg-dev <ksg.dev.data@gmail.com>
2025-08-18 16:44:22 -0400

    delete excess urls, fix system create admin path

--- projects/urls.py
+++ projects/urls.py
@@ -10,16 +10,6 @@ urlpatterns = [
   
     path("systems/", views.EnhancedLearningSystemListView.as_view(), name="system_list"),
     path("systems/<slug:slug>/", views.LearningSystemControlInterfaceView.as_view(), name="system_detail"),
-    # Testing new interface w GitHub Data
-    # path("test/<slug:slug>/", views.SystemControlInterfaceView.as_view(), name="system_detail_test"),
-
-    # path("systems/dashboard/", views.SystemsDashboardView.as_view(), name="systems_dashboard"),
-
-    # System Detail and management
-    # Handled by aura-admin
-    # path("systems/create/", views.SystemModuleCreateView.as_view(), name="system_create"),
-    # path("systems/<slug:slug>/edit/", views.SystemModuleUpdateView.as_view(), name="system_update"),
-    # path("systems/<slug:slug>/delete/", views.SystemModuleDeleteView.as_view(), name="system_delete"),
 
     # System type and technology views
     path("types/", views.SystemTypesOverviewView.as_view(), name="system_types_overview"),
@@ -35,11 +25,4 @@ urlpatterns = [
     path('github/sync/', views.GitHubSyncView.as_view(), name='github_sync'),
     path('github/test/', views.GitHubIntegrationTestView.as_view(), name='github_test'),
     path('chartjs-test/', TemplateView.as_view(template_name='projects/chartjs_test.html'), name='chartjs_test'),
-    # path('github/repository/<str:repo_name>/', views.GitHubRepositoryDetailView.as_view(), name='github_repo_detail'),
-
-
-    # Dashboard views to dial in for later enhancement
-    # path("dashboard/", views.EnhancedSystemsDashboardView.as_view(), name="systems_dashboard"),
-    # path("learning/", views.LearningJourneyDashboardView.as_view(), name="learning_dashboard"),
-    # path("landing/", views.PortfolioLandingDashboardView.as_view(), name="landing_dashboard"),
 ]
