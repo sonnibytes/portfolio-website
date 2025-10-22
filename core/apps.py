e0c05df2cc03e0f388674cde341041d1d51dc21a
ksg-dev <ksg.dev.data@gmail.com>
2025-10-22 06:27:03 -0400

    import signals to core when app is ready

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
+
+    # Import signals when app is ready
+    def ready(self):
+        import core.signals
