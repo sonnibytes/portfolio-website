58878abdd2838cc407df008da254c5d7ebc9d80f
ksg-dev <ksg.dev.data@gmail.com>
2025-10-22 06:27:43 -0400

    import signals to projects app when ready

--- projects/apps.py
+++ projects/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ProjectsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "projects"
+
+    # Import signals when app is ready
+    def ready(self):
+        import core.signals
